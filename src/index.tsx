c747d333f24ae81d747ae6b5b36cde6daacd174f
rod773 <rodrigoarcadio1@gmail.com>
2024-10-22 20:15:12 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import { CssBaseline } from '@mui/material';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './redux/store';
-import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -17,8 +16,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
